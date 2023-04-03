0c038cd8a3dfbf84e790f5fc1a9b35b7168bb568
Mateus Zanetti Camargo Penteado <matezcp@hotmail.com>
2023-04-03 20:05:39 -0300

    Adicionar arquivos de projeto.

---
+++ stock-quote-alert/Program.cs
@@ -0,0 +1,51 @@
+﻿using stock_quote_alert.Dto;
+using stock_quote_alert.Services;
+using System.Globalization;
+
+namespace stock_quote_alert
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string? stockQuote = null;
+            double? stockSellPrice = null;
+            double? stockBuyPrice = null;
+
+            try
+            {
+                stockQuote = args[0];
+                stockSellPrice = Double.Parse(args[1], CultureInfo.InvariantCulture);
+                stockBuyPrice = Double.Parse(args[2], CultureInfo.InvariantCulture);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(
+                    "Formato Inválido!\n" +
+                    "Verifique se o ticker e os preços de negociação de compra estão no formato adequado.\n" +
+                    "Exemplo de Formato Adequado: PETR4 22.67 22.59");
+                return;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Paramêtro faltante.\n" +
+                    "Deve ser passado o ticker, o preço de venda e o preço de compra separados por espaço!\n" +
+                    "Exemplo de Formato Adequado: PETR4 22.67 22.59");
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Parâmetros Inválidos!\n" + "Error: " + ex.ToString());
+                return;
+            }
+
+            ApiService apiService = new ApiService();
+
+            //Console.WriteLine(stockQuote);
+            //Console.WriteLine(stockSellPrice);
+            //Console.WriteLine(stockBuyPrice);
+
+            Console.WriteLine(await apiService.GetStockPrice(stockQuote));
+        }
+    }
+}
---
+++ stock-quote-alert/Services/ApiService.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using stock_quote_alert.Dto;
+
+namespace stock_quote_alert.Services
+{
+    public class ApiService
+    {
+        public async Task<double?> GetStockPrice(string stockQuote)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync("https://brapi.dev/api/quote/" + stockQuote);
+
+            double? price = null;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var jsonObject = JsonConvert.DeserializeObject<dynamic>(content);
+                price = Double.Parse(jsonObject?.results[0].regularMarketPrice.ToString());
+            }
+            else if(response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                Console.WriteLine("Código de negociação não encontrado, verifique o código passado ao sistema!");
+            }
+            else
+            {
+                Console.WriteLine("Request failed with status code " + response.StatusCode);
+            }
+
+            return price;
+        }
+    }
+}
